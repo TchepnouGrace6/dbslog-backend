63ce9427cd5ecd54d5587ff3102986d8c4d500dd
Grace <grace6tchepnou@gmail.com>
2025-07-18 17:11:41 +0100

    Correction import contrôleur

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authcontroller');
 
 // Visiteur s'inscrit
 router.post('/visiteur/register', authController.registerVisiteur);
