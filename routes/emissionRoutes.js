28ae636ca13ed96ab4171827c414324af1fd4301
Grace <grace6tchepnou@gmail.com>
2025-07-20 21:50:13 +0100

    Correction du point d'entrée

--- routes/emissionRoutes.js
+++ routes/emissionRoutes.js
@@ -6,6 +6,7 @@ const { verifyToken, isAdmin } = require('../middlewares/authmiddleware');
 // Routes CRUD accessibles uniquement à l'administrateur
 router.post('/createemission', verifyToken, isAdmin, emissionController.createEmission);
 router.get('/afficheemission', verifyToken, isAdmin, emissionController.getAllEmissions);
+router.get("/categorie/:id", emissionController.getByCategorie);
 router.get('/:id', verifyToken, isAdmin, emissionController.getEmissionById);
 router.put('/:id', verifyToken, isAdmin, emissionController.updateEmission);
 router.delete('/:id', verifyToken, isAdmin, emissionController.deleteEmission);
